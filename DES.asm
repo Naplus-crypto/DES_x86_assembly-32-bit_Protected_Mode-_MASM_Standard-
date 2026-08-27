73f4e6cfac63d454da11302ff900d9bc8e69ff68
Naplus-crypto <patiohat@gmail.com>
2026-08-27 13:03:57 +0700

    Create DES.asm

---
+++ DES.asm
@@ -0,0 +1,6 @@
+; -------------------------------------------------------------
+; Project: DES Encryption/Decryption in Assembly
+; Course: Computer Organization and Assembly Language
+; Author: Patiphat Prathumwan (68010627)
+; License: GNU General Public License v3.0 (GPLv3)
+; -------------------------------------------------------------
